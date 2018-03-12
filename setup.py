3c5762fda006468fd54a4f690139c5274842c207
Angelo Corsaro <angelo@icorsaro.net>
2018-03-12 16:14:29 +0100

    updated config.py to reflect name change

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(
-    name='pycham',
+    name='python-cdds',
     version='0.1.0',
     packages=['cdds', ],
     author='kydos',
-    url='https://github.com/atolab/pycham',
+    url='https://github.com/atolab/python-cdds',
     install_requires=['jsonpickle']
 )
