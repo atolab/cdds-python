24ca142945dd54a2dff45cafce6897790d6e8078
gabrik <gabriele.baldoni@gmail.com>
2018-03-15 14:21:40 +0100

    test

--- cdds/reader.py
+++ cdds/reader.py
@@ -11,7 +11,7 @@ def trampoline_on_liveliness_changed(r, s, a):
 def trampoline_on_data_available(r, a):
     print("[python-cdds]:>>  Dispatching Data Available ")
     import  threading
-    threading.Thread(target=Runtime.dispatch_data_listener, args=(r)).start()
+    threading.Thread(target=Runtime.dispatch_data_listener, args=[r]).start()
     #Runtime.dispatch_data_listener(r)
 
 @SUBSCRIPTION_MATCHED_PROTO
